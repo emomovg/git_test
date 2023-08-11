4106f76e3546eee80c5b16164a7df44cac70864c
Emomov <emomovgolibjon@gmail.com>
2023-08-11 14:56:20 +0500

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,14 +5,14 @@ namespace App\Http;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
-{
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array<int, class-string|string>
-     */
+//{
+//    /**
+//     * The application's global HTTP middleware stack.
+//     *
+//     * These middleware are run during every request to your application.
+//     *
+//     * @var array<int, class-string|string>
+//     */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
